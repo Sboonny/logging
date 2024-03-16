a97e9a4936b0f28b9adbeccb361f5b47574fda73
Sboonny <muhammedelruby@gmail.com>
2024-03-16 10:30:25 +0200

    create post request

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -4,25 +4,33 @@ type EventAction = {
     object: string;
     name: string;
   };
-  type Metadata = {
+
+type Metadata = {
     redirect: string;
     description: string;
     x_request_id: string;
   };
   
   // This type is used to define the shape of our data.
-  // You can use a Zod schema here if you want.
+  // We can use a Zod schema here.
   export type Event = {
     id: string;
     object: string;
     actor_id: string;
     actor_name: string;
     group: string;
-    action: EventAction;
+    action_id: string;
     target_id: string;
     target_name: string;
     location: string;
     occurred_at: string;
-    metadata: Metadata;
+    metadata_id: string;
+    createdAt: Date;
+    updatedAt: Date;
   }
   
+
+  export type EventWithRelations = Event & {
+    action: EventAction;
+    metadata: Metadata;
+  };

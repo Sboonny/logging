94207b5df9847db189c5ee585e77c8ed1167ce8f
Sboonny <muhammedelruby@gmail.com>
2024-03-17 10:57:38 +0200

    fix the post error

--- src/server/route/event.ts
+++ src/server/route/event.ts
@@ -1,49 +1,50 @@
-import type { FastifyInstance, FastifyPluginOptions, FastifyError } from "fastify";
-import { Event, EventWithRelations } from "~/interface";
+import type {
+  FastifyInstance,
+  FastifyPluginOptions,
+  FastifyError,
+} from "fastify";
+import { type Event } from "~/interface";
 
-export function eventRoutes(fastify: FastifyInstance, _options: FastifyPluginOptions, done: (error?: FastifyError) => void) {
-
-    fastify.get('/event', async (_request, _reply) => {
-        const events = await fastify.prisma.event.findMany({
-            take: 3,
-        });
-
-        return events;
+export function eventRoutes(
+  fastify: FastifyInstance,
+  _options: FastifyPluginOptions,
+  done: (error?: FastifyError) => void,
+) {
+  fastify.get("/event", async (_request, _reply) => {
+    const events = await fastify.prisma.event.findMany({
+      take: 3,
     });
 
-    // fastify.post('/event', async (request, _reply) => {
-    //     const body = request.body as EventWithRelations;
-    //     const actionId = body.action_id;
-    //     const metadata_id = body.metadata_id;
+    return events;
+  });
 
-    //     const action = await fastify.prisma.eventAction.findUniqueOrThrow({
-    //         where: { action_id: actionId },
-    //     });
+  fastify.post("/event", async (request, _reply) => {
+    const body = request.body as Exclude<Event, "createdAt" | "updatedAt">;
+    const createdEvent = await fastify.prisma.event.create({
+      data: {
+        object: body.object,
+        actor_id: body.actor_id,
+        actor_name: body.actor_name,
+        group: body.group,
+        target_id: body.target_id,
+        target_name: body.target_name,
+        location: body.location,
+        occurred_at: body.occurred_at,
+        action: {
+          id: body.action.id,
+          object: body.action.object,
+          name: body.action.name,
+        },
+        metadata: {
+          redirect: body.metadata.redirect,
+          description: body.metadata.description,
+          x_request_id: body.metadata.x_request_id,
+        },
+      },
+    });
 
-    //     const metadata = await fastify.prisma.metadata.findUniqueOrThrow({
-    //         where: { x_request_id: metadata_id },
-    //     });
+    return createdEvent;
+  });
 
-    //     const createdEvent = await fastify.prisma.event.create({
-        // ToDo: fix the type error with it being undefined an null
-    //         data: {
-    //             object: body.object,
-    //             actor_id: body.actor_id,
-    //             actor_name: body.actor_name,
-    //             group: body.group,
-    //             action_id: body.action_id,
-    //             target_id: body.target_id,
-    //             target_name: body.target_name,
-    //             location: body.location,
-    //             occurred_at: body.occurred_at,
-    //             metadata_id: body.metadata_id,
-    //             action: { connect: { action_id: action.action_id } },
-    //             metadata: { connect: { x_request_id: metadata.x_request_id } },
-    //         },
-    //     });
-
-    //     return createdEvent;
-    // });
-
-    done();
+  done();
 }

5631cda89c60da23bef000764e95bb85710864d2
Sboonny <muhammedelruby@gmail.com>
2024-03-17 11:59:50 +0200

    next start when nodeman runs, WSL

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,15 +3,29 @@ import Fastify, {
   type FastifyHttpOptions,
   type RawServerDefault
 } from 'fastify';
+import { eventRoutes } from './route/event';
+import prismaPlugin from './db';
 
-
-
-export const build = async (
+const build = async (
   options: FastifyHttpOptions<RawServerDefault, FastifyBaseLogger> = {}
 ) => {
     const fastify = Fastify(options);
 
-
+    await fastify.register(prismaPlugin);
+    await fastify.register(eventRoutes);
 
   return fastify;
 };
+
+const start = async () => {
+  const fastify = await build();
+  try {
+    fastify.log.info(`Starting server on port 8000`);
+    await fastify.listen({ port: 8000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+void start();

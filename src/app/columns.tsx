45cc61c59165d9dbad4c5dbe9a64af7dee285697
Sboonny <muhammedelruby@gmail.com>
2024-03-17 10:44:29 +0200

    add an avatar in the table

--- src/app/columns.tsx
+++ src/app/columns.tsx
@@ -1,13 +1,24 @@
 "use client";
 
 import type { ColumnDef } from "@tanstack/react-table";
+import { Avatar, AvatarImage, AvatarFallback } from "~/components/ui/avatar";
 import type { Event } from "~/interface";
-
 
 export const columns: ColumnDef<Event>[] = [
   {
     accessorKey: "target_name",
     header: "ACTION",
+    cell: ({ row }) => {
+      return (
+        <div className="flex items-center gap-4">
+          <Avatar>
+            <AvatarImage src="" />
+            <AvatarFallback className="bg-gradient-to-br from-[#F3994A] to-[#B325E2] text-white font-bold">{row.original.actor_name.charAt(0)}</AvatarFallback>
+          </Avatar>
+          {row.original.target_name}
+        </div>
+      );
+    },
   },
   {
     accessorKey: "actor_name",
@@ -17,11 +28,11 @@ export const columns: ColumnDef<Event>[] = [
     accessorKey: "occurred_at",
     header: "DATE",
     cell: ({ row }) => {
-        const formatted = new Intl.DateTimeFormat("en-US", {
-            dateStyle: "medium",
-            timeStyle: "short",
-            }).format(new Date(row.original.occurred_at));
-        return  formatted
-    }
+      const formatted = new Intl.DateTimeFormat("en-US", {
+        dateStyle: "medium",
+        timeStyle: "short",
+      }).format(new Date(row.original.occurred_at));
+      return formatted;
+    },
   },
 ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,111 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
-import { EventTable } from "./event-table";
-async function getData() {
-  "use server"
-  // didn't create the route yet, I am getting invalid URL
-  // Cause: TypeError [ERR_INVALID_URL]: Invalid URL
-  const res = await fetch('/api/event', {
-      method: 'GET',
-  })
-console.log("res:", res)
-  if (!res.ok) {
-      throw new Error('Failed to fetch data')
-  }
+import { type Event } from "~/interface";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
 
-  return res.json();
+async function getData(): Promise<Event[]> {
+  return [
+    {
+      id: "evt_15B56WILKW5K",
+      object: "event",
+      actor_id: "user_3VG74289PUA2",
+      actor_name: "Ali Salah",
+      group: "instatus.com",
+      action: {
+        id: "evt_action_PGTD81NCAOQ2",
+        object: "event_action",
+        name: "user.login_succeeded",
+      },
+      target_id: "user_DOKVD1U3L030",
+      target_name: "ali@instatus.com",
+      location: "105.40.62.95",
+      occurred_at: "2022-01-05T14:31:13.607Z",
+      metadata: {
+        redirect: "/setup",
+        description: "User login succeeded.",
+        x_request_id: "req_W1Y13QOHMI5H",
+      },
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: "evt_15B56WILKW5K",
+      object: "event",
+      actor_id: "user_3VG74289PUA2",
+      actor_name: "Ali Salah",
+      group: "instatus.com",
+      action: {
+        id: "evt_action_PGTD81NCAOQ2",
+        object: "event_action",
+        name: "user.searched_activity_log_events",
+      },
+      target_id: "user_DOKVD1U3L030",
+      target_name: "ali@instatus.com",
+      location: "105.40.62.95",
+      occurred_at: "2022-01-05T14:31:13.607Z",
+      metadata: {
+        redirect: "/setup",
+        description: "User login succeeded.",
+        x_request_id: "req_W1Y13QOHMI5H",
+      },
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: "evt_15B56WILKW5K",
+      object: "event",
+      actor_id: "user_3VG74289PUA2",
+      actor_name: "Omar",
+      group: "instatus.com",
+      action: {
+        id: "evt_action_PGTD81NCAOQ2",
+        object: "event_action",
+        name: "incident.create_succeeded",
+      },
+      target_id: "user_DOKVD1U3L030",
+      target_name: "omar@instatus.com",
+      location: "105.40.62.95",
+      occurred_at: "2022-01-05T14:31:13.607Z",
+      metadata: {
+        redirect: "/setup",
+        description: "User login succeeded.",
+        x_request_id: "req_W1Y13QOHMI5H",
+      },
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: "evt_15B56WILKW5K",
+      object: "event",
+      actor_id: "user_3VG74289PUA2",
+      actor_name: "Barra",
+      group: "instatus.com",
+      action: {
+        id: "evt_action_PGTD81NCAOQ2",
+        object: "event_action",
+        name: "user.invited_teammate",
+      },
+      target_id: "user_DOKVD1U3L030",
+      target_name: "barra@instatus.com",
+      location: "105.40.62.95",
+      occurred_at: "2022-01-05T14:31:13.607Z",
+      metadata: {
+        redirect: "/setup",
+        description: "User login succeeded.",
+        x_request_id: "req_W1Y13QOHMI5H",
+      },
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ];
 }
 
 export default async function HomePage() {
+  const data = await getData();
+
   return (
     <div className="container mx-auto py-10">
-      <EventTable getData={getData} />
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
